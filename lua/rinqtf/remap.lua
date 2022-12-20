011b1fcf8027fd944e58c5c190d1c152ef2d0d81
rinqtf <rinqtf@gmail.com>
2022-12-20 21:07:11 +0300

    add save keymap

--- lua/rinqtf/remap.lua
+++ lua/rinqtf/remap.lua
@@ -55,3 +55,5 @@ keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left
 keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+keymap.set("n", "<leader>w", ":w<CR>", { silent = true })
