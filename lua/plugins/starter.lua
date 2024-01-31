7fb544be409d1eb21e4c177c03001da20ac8878d
rinqtf <rinqtf@gmail.com>
2024-01-31 11:17:17 +0300

    add starter

---
+++ lua/plugins/starter.lua
@@ -0,0 +1,21 @@
+return {
+	"echasnovski/mini.starter",
+	version = false,
+	config = function()
+		local starter = require("mini.starter")
+		local new_section = function(name, action, section)
+			return { name = name, action = action, section = section }
+		end
+		starter.setup({
+			evaluate_single = true,
+			items = {
+				new_section("Find file", "Telescope find_files", "Telescope"),
+				new_section("Recent files", "Telescope oldfiles", "Telescope"),
+				new_section("Grep text", "Telescope live_grep", "Telescope"),
+				new_section("Lazy", "Lazy", "Config"),
+				new_section("New file", "ene | startinsert", "Built-in"),
+				new_section("Quit", "qa", "Built-in"),
+			},
+		})
+	end,
+}
