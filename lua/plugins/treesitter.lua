4d2839ad1391cb80491e583b890597cd5fca9a53
rinqtf <rinqtf@gmail.com>
2024-01-31 09:47:42 +0300

    format treesitter file

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,51 +1,51 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  config = function()
-    require'nvim-treesitter.configs'.setup {
-      ensure_installed = {
-        "bash",
-        "c",
-        "html",
-        "javascript",
-        "json",
-        "jsonc",
-        "lua",
-        "luadoc",
-        "luap",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "regex",
-        "tsx",
-        "typescript",
-        "vim",
-        "vimdoc",
-        "css",
-        "yaml",
-        "rust",
-      },
-      sync_install = false,
-      auto_install = false,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      autotag = {
-        enable = true,
-      },
-      indent = {
-        enable = true
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
-    }
-  end
+	"nvim-treesitter/nvim-treesitter",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"bash",
+				"c",
+				"html",
+				"javascript",
+				"json",
+				"jsonc",
+				"lua",
+				"luadoc",
+				"luap",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"query",
+				"regex",
+				"tsx",
+				"typescript",
+				"vim",
+				"vimdoc",
+				"css",
+				"yaml",
+				"rust",
+			},
+			sync_install = false,
+			auto_install = false,
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+			autotag = {
+				enable = true,
+			},
+			indent = {
+				enable = true,
+			},
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-space>",
+					node_incremental = "<C-space>",
+					scope_incremental = false,
+					node_decremental = "<bs>",
+				},
+			},
+		})
+	end,
 }
