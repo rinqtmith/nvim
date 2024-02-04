825fd538c4865f1eea42608c1a4a389f284032f9
rinqtf <rinqtf@gmail.com>
2024-02-04 22:49:04 +0300

    add jsonls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -140,7 +140,7 @@ return {
 			})
 			require("mason").setup()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "rust_analyzer", "tsserver", "html", "cssls" },
+				ensure_installed = { "lua_ls", "rust_analyzer", "tsserver", "html", "cssls", "jsonls" },
 				handlers = {
 					lsp_zero.default_setup,
 					lua_ls = function()
