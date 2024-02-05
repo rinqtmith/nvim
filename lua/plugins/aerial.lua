057b859fdccdc0a79b5f0848dd8fb56d61a8832c
rinqtf <rinqtf@gmail.com>
2024-02-06 00:16:40 +0300

    set aerial backend to lsp

--- lua/plugins/aerial.lua
+++ lua/plugins/aerial.lua
@@ -6,10 +6,12 @@ return {
 	},
 	config = function()
 		require("aerial").setup({
+			backends = { "lsp" },
 			layout = {
 				max_width = { 70, 0.2 },
 				min_width = 40,
 			},
+			filter_kind = false,
 		})
 	end,
 }
