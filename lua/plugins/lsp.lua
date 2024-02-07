e87c4af3a1142c6f93e3bf39f54245c6ef41ba76
rinqtf <rinqtf@gmail.com>
2024-02-07 23:33:31 +0300

    add toml lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -140,7 +140,7 @@ return {
 			})
 			require("mason").setup()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "rust_analyzer", "tsserver", "html", "cssls", "jsonls" },
+				ensure_installed = { "lua_ls", "rust_analyzer", "tsserver", "html", "cssls", "jsonls", "taplo" },
 				handlers = {
 					lsp_zero.default_setup,
 					lua_ls = function()
