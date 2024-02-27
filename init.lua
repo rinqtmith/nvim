267aad23626a9d73d046a7053f9863823be71ec2
rinqtf <rinqtf@gmail.com>
2024-02-27 12:08:48 +0300

    add colorscheme

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
 require("rinqtf.spec")
 require("rinqtf.core")
+spec("rinqtf.plugins.colorscheme")
 require("rinqtf.lazy")
--- lua/rinqtf/plugins/colorscheme.lua
+++ lua/rinqtf/plugins/colorscheme.lua
@@ -1,30 +1,14 @@
 return {
-	"rebelot/kanagawa.nvim",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		require("kanagawa").setup({
-			-- dimInactive = true,
-			transparent = true,
-			overrides = function(colors)
-				local theme = colors.theme
-				return {
-					NormalFloat = { bg = "none" },
-					FloatBorder = { bg = "none" },
-					FloatTitle = { bg = "none" },
-
-					-- Save an hlgroup with dark background and dimmed foreground
-					-- so that you can use it where your still want darker windows.
-					-- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
-					NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-
-					-- Popular plugins that open floats will link to NormalFloat by default;
-					-- set their background accordingly if you wish to keep them dark and borderless
-					LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-					MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-				}
-			end,
-		})
-		vim.cmd("colorscheme kanagawa")
-	end,
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  opts = {
+    transparent_background = true,
+    integrations = {
+    },
+  },
+  config = function(_, opts)
+    require('catppuccin').setup(opts)
+    vim.cmd.colorscheme 'catppuccin-mocha'
+  end
 }
