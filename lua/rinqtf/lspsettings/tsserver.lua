1218d2bb71b3178db3d4564c1272581b98848a75
rinqtf <rinqtf@gmail.com>
2024-02-27 21:57:57 +0300

    add server configurations

---
+++ lua/rinqtf/lspsettings/tsserver.lua
@@ -0,0 +1,37 @@
+local wk = require("which-key")
+wk.register({
+	["<leader>lt"] = {
+		name = "+tsserver",
+		r = {
+			function()
+				vim.lsp.buf.code_action({
+					apply = true,
+					context = {
+						only = { "source.removeUnused.ts" },
+						diagnostics = {},
+					},
+				})
+			end,
+			"Remove Unused Imports",
+		},
+		o = {
+			function()
+				vim.lsp.buf.code_action({
+					apply = true,
+					context = {
+						only = { "source.organizeImports.ts" },
+						diagnostics = {},
+					},
+				})
+			end,
+			"Organize Imports",
+		},
+	},
+})
+return {
+	settings = {
+		completions = {
+			completeFunctionCalls = true,
+		},
+	},
+}
