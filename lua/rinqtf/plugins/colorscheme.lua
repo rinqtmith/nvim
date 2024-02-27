2e2cd39713afb1dd7d220b9e7c8d9116a035e1aa
rinqtf <rinqtf@gmail.com>
2024-02-27 21:22:10 +0300

    add integrations to colorscheme

--- lua/rinqtf/plugins/colorscheme.lua
+++ lua/rinqtf/plugins/colorscheme.lua
@@ -4,7 +4,28 @@ return {
 	priority = 1000,
 	opts = {
 		transparent_background = true,
-		integrations = {},
+		integrations = {
+			mason = true,
+			native_lsp = {
+				enabled = true,
+				virtual_text = {
+					errors = { "italic" },
+					hints = { "italic" },
+					warnings = { "italic" },
+					information = { "italic" },
+				},
+				underlines = {
+					errors = { "underline" },
+					hints = { "underline" },
+					warnings = { "underline" },
+					information = { "underline" },
+				},
+				inlay_hints = {
+					background = true,
+				},
+			},
+			which_key = false,
+		},
 	},
 	config = function(_, opts)
 		require("catppuccin").setup(opts)
