0fb758a7173248d7894b17c898ce1c1553a63d52
rinqtf <rinqtf@gmail.com>
2024-02-27 17:26:48 +0300

    add telescope

--- lua/rinqtf/plugins/telescope.lua
+++ lua/rinqtf/plugins/telescope.lua
@@ -1,21 +1,121 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	dependencies = {
-		{ "nvim-lua/plenary.nvim" },
-		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-	},
-	config = function()
-		require("telescope").setup({
-			extensions = {
-				fzf = {
-					fuzzy = true,
-					override_generic_sorter = true,
-					override_file_sorter = true,
-					case_mode = "smart_case",
-				},
-			},
-		})
-		require("telescope").load_extension("fzf")
-		require("telescope").load_extension("noice")
-	end,
+  "nvim-telescope/telescope.nvim",
+  dependencies = {
+    { "nvim-lua/plenary.nvim" },
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make", lazy = true },
+  },
+  config = function()
+    local wk = require "which-key"
+    wk.register {
+      ["<leader>bb"] = { "<cmd>Telescope buffers previewer=false<cr>", "Find" },
+      ["<leader>fb"] = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
+      ["<leader>fc"] = { "<cmd>Telescope colorscheme<cr>", "Colorscheme" },
+      ["<leader>ff"] = { "<cmd>Telescope find_files<cr>", "Find files" },
+      -- ["<leader>fp"] = { "<cmd>lua require('telescope').extensions.projects.projects()<cr>", "Projects" },
+      ["<leader>ft"] = { "<cmd>Telescope live_grep<cr>", "Find Text" },
+      ["<leader>fh"] = { "<cmd>Telescope help_tags<cr>", "Help" },
+      ["<leader>fl"] = { "<cmd>Telescope resume<cr>", "Last Search" },
+      ["<leader>fr"] = { "<cmd>Telescope oldfiles<cr>", "Recent File" },
+    }
+
+    local icons = require "rinqtf.icons"
+    local actions = require "telescope.actions"
+
+    require("telescope").setup({
+      defaults = {
+        prompt_prefix = icons.ui.Telescope .. " ",
+        selection_caret = icons.ui.Forward .. " ",
+        entry_prefix = "   ",
+        initial_mode = "insert",
+        selection_strategy = "reset",
+        path_display = { "smart" },
+        color_devicons = true,
+        vimgrep_arguments = {
+          "rg",
+          "--color=never",
+          "--no-heading",
+          "--with-filename",
+          "--line-number",
+          "--column",
+          "--smart-case",
+          "--hidden",
+          "--glob=!.git/",
+        },
+        mappings = {
+          i = {
+            ["<C-n>"] = actions.cycle_history_next,
+            ["<C-p>"] = actions.cycle_history_prev,
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-k>"] = actions.move_selection_previous,
+          },
+          n = {
+            ["<esc>"] = actions.close,
+            ["j"] = actions.move_selection_next,
+            ["k"] = actions.move_selection_previous,
+            ["q"] = actions.close,
+          },
+        },
+      },
+      pickers = {
+        live_grep = {
+          theme = "dropdown",
+        },
+
+        grep_string = {
+          theme = "dropdown",
+        },
+
+        find_files = {
+          theme = "dropdown",
+          previewer = false,
+        },
+        buffers = {
+          theme = "dropdown",
+          previewer = false,
+          initial_mode = "normal",
+          mappings = {
+            i = {
+              ["<C-d>"] = actions.delete_buffer,
+            },
+            n = {
+              ["dd"] = actions.delete_buffer,
+            },
+          },
+        },
+        planets = {
+          show_pluto = true,
+          show_moon = true,
+        },
+        colorscheme = {
+          enable_preview = true,
+        },
+        lsp_references = {
+          theme = "dropdown",
+          initial_mode = "normal",
+        },
+        lsp_definitions = {
+          theme = "dropdown",
+          initial_mode = "normal",
+        },
+        lsp_declarations = {
+          theme = "dropdown",
+          initial_mode = "normal",
+        },
+        lsp_implementations = {
+          theme = "dropdown",
+          initial_mode = "normal",
+        },
+      },
+      extensions = {
+        fzf = {
+          fuzzy = true,
+          override_generic_sorter = true,
+          override_file_sorter = true,
+          case_mode = "smart_case",
+        },
+      },
+    })
+    require("telescope").load_extension("fzf")
+    -- require("telescope").load_extension("noice")
+  end,
 }
