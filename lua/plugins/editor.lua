440b370b047612d00db65c3affa6f625b8f1bff4
rinqtf <rinqtf@gmail.com>
2024-03-12 13:13:36 +0300

    initial

---
+++ lua/plugins/editor.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/flash.nvim",
+  vscode = false,
+  opts = {
+    search = {
+      forward = true,
+      multi_window = false,
+      wrap = false,
+      incremental = true,
+    },
+  },
+}
